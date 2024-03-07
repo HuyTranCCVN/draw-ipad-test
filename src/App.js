494729ad14ea0e011606201cafec1976ac96010b
huy.tran@codecomplete.jp <Nhathuy2401>
2024-03-07 09:36:44 +0700

    ref input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import React , {useEffect,useRef} from 'react';
 import './App.css';
 
 function App() {
+  const inputRef = useRef(null);
+  useEffect(() => {
+    inputRef.current.focus();
+  },[])
   return (
     <div className="App">
       <input
         placeholder={"Placeholder"}
         className={"input"} 
+        ref={inputRef}
       />
     </div>
   );
